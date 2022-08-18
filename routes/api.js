4f3a2d0f876bda88bb8346556adf33208ec07aed
DARK CRIME <86085229+DARKCRIME1@users.noreply.github.com>
2022-08-18 12:30:30 +0530

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -277,7 +277,7 @@ router.get("/news", async (req, res) => {
         .then(response => {
 
             results = [];
-            const $ = cheerio.load(response.data);
+            const $ = cheerio.load(response.data, { decodeEntities: false });
 
             $('div.news-story div.story-text h2').find('a').each((i, elem) => {
                 let news = {
