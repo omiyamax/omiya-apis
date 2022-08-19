f8c9418c881376abb38d7486e72a366856a3cc7c
DARK CRIME <86085229+DARKCRIME1@users.noreply.github.com>
2022-08-19 19:10:32 +0530

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -286,16 +286,14 @@ function _0xd05a(_0x14cd43,_0x82ffd4){const _0x3d7e6=_0x4cd8();return _0xd05a=fu
 
 
 router.get("/hirunews/", async (req, res) => {
-	var url = req.query.url
+	var url = req.params.q
 	var Apikey = req.query.apikey
 	
   
     if(!Apikey) return res.json(loghandler.notparam)
     if(listkey.includes(Apikey)){
 	
-	if (!url) {
-		res.json(errnya)
-	} else {
+	
     
     
 	    axios.get(url)
@@ -331,7 +329,7 @@ router.get("/hirunews/", async (req, res) => {
 
 });
 		
-	}
+	
 	} else {
         res.json(loghandler.invalidKey)
     }
