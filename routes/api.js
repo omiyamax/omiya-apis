bef0e33ce86c9116249a08a937d92b96ba4409f3
DARK CRIME <86085229+DARKCRIME1@users.noreply.github.com>
2022-08-19 16:56:40 +0530

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -33,7 +33,6 @@ var emoji = new EmojiAPI();
 var router  = express.Router();
 var { TiktokDownloader } = require('../lib/tiktok.js')
 var baiscopelk = require('../lib/baiscopelk.js');
-var news = require('..lib/news.js')
 var { color, bgcolor } = require(__path + '/lib/color.js');
 var { fetchJson } = require(__path + '/lib/fetcher.js');
 var options = require(__path + '/lib/options.js');
